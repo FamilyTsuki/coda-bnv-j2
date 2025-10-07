c099b317acebf3d14b0a2ff53f1ec7fb0b75bcbc
FamilyTsuki <alban.elie590@gmail.com>
2025-10-07 12:09:33 +0000

    salut

---
+++ ex-5/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+int v1 = 10;
+int v2 = 7;
+double v3 = (v1 + v2) / 3;
+printf("La moyenne de 12 et 7 est égale à %.2f\n", v3);
+exit(0);
+
+}
