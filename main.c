0773ced8e48458e1e7cde35ff2c6b95666bb747b
FamilyTsuki <alban.elie590@gmail.com>
2025-10-07 13:43:24 +0000

    ok

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+printf("%s", "program : bonjour quelle est votre pronon");
+char str;
+
+scanf("%s", &str);
+printf("quelle age a tu %s", &str);
+exit(0);
+
+}
